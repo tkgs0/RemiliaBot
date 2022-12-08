c1e05b11ed38d5ff567dda817a95a8dca025a14c
tkgs0 <hgc.sakuya@ya.ru>
2022-12-08 12:26:00 +0000

    commit

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -6,7 +6,7 @@ from telegram.ext import (
 
 from utils.log import logger
 from config import SETU
-from .utils import get_setu
+from .rdsetu import get_setu
 
 
 def run(application):
@@ -16,17 +16,41 @@ def run(application):
 
 async def setu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info(f'[{update.message.chat.type.upper()}]({update.message.chat_id}) {context._user_id}: {update.message.text}')
-    tag = context.args
+    if (tag := context.args) and is_number(tag[0]):
+        num = int(tag[0])
+        tag.remove(tag[0])
+    else:
+        tag = context.args
+        num = 6
     content = await get_setu(
         tag=tag,
         r18=SETU['r18'],
+        num=num,
         pixproxy=SETU['pixproxy']
     )
-    if content[1]:
+    if content[1] == 2:
+        await update.message.reply_media_group(
+            media = content[0],
+            caption = content[2] if content[2] else None)
+    elif content[1]:
+        pic = content[0]
         await update.message.reply_photo(
-            photo = content[0],
-            caption = content[1]
-        )
+            photo = pic[0],
+            caption = pic[1] + ('\n'+content[2] if content[2] else ''))
     else:
-        await update.message.reply_text(content[0])
+        await update.message.reply_text(str(content[0]))
 
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
