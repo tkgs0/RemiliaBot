bdf0b85975212748203f2c5486047d519f1232b7
tkgs0 <hgc.sakuya@ya.ru>
2022-12-12 14:15:48 +0000

    commit

--- src/config.py
+++ src/config.py
@@ -29,6 +29,8 @@ CHATGPT:
   password: ''
   Authorization: ''
   session_token: ''
+  accept_language: 'en-US,en'
+  user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 
 """.strip()
 
