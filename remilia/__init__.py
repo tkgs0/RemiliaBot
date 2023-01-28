29a680ce18a102dbcca8b08ee8d728666d334731
tkgs0 <hgc.sakuya@ya.ru>
2023-01-28 07:40:01 +0000

    :bug:

--- remilia/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
-from remilia.src import run
-
-if __name__ == '__main__':
-    run()
---
+++ remilia/__main__.py
@@ -0,0 +1,4 @@
+from remilia.src import run
+
+if __name__ == '__main__':
+    run()
